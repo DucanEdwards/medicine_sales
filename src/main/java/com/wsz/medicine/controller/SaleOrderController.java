b0147e311e3972bfcf7767986ef3ca2d905caf83
Duncan Edwards <305185235@qq.com>
2021-05-02 12:03:47 +0800

    完成通用返回类

--- src/main/java/com/wsz/medicine/controller/SaleOrderController.java
+++ src/main/java/com/wsz/medicine/controller/SaleOrderController.java
@@ -1,6 +1,7 @@
 package com.wsz.medicine.controller;
 
 import com.wsz.medicine.domain.SaleOrder;
+import com.wsz.medicine.resp.CommonResp;
 import com.wsz.medicine.service.SaleOrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,11 @@ public class SaleOrderController {
     private SaleOrderService saleOrderService;
 
     @GetMapping("/saleorder/list")
-    public List<SaleOrder> list() {
-        return saleOrderService.list();
+    public CommonResp list() {
+        CommonResp<List<SaleOrder>> saleOrderCommonResp = new CommonResp<>();
+        List<SaleOrder> list=saleOrderService.list();
+        saleOrderCommonResp.setContent(list);
+        return saleOrderCommonResp;
     }
 
 }
