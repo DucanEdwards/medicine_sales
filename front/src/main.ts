75e48db765d792d218ad036c455b001a81249789
Duncan Edwards <305185235@qq.com>
2021-05-02 18:04:02 +0800

    集成组件库

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).mount('#app')
+const app=createApp(App);
+app.use(store).use(router).use(ElementPlus).use(Antd).mount('#app')
